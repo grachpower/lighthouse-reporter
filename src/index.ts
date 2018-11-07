56f54ed8004165988f51895eca3add71a878c727
DESKTOP-DA70CJK\Artem.Andreev <Artem.Andreev@clouty.ru>
2018-11-07 14:07:56 +0300

    add docker configs

--- src/index.ts
+++ src/index.ts
@@ -22,41 +22,43 @@ const telegaApi = new TelegaApi(chatId, botToken);
 function launchChromeAndRunLighthouse(url, opts, config = null): Promise<RootObject> {
     return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
         opts.port = chrome.port;
-        return lighthouse(url, opts, config).then(results => {
-            // use results.lhr for the JS-consumeable output
-            // https://github.com/GoogleChrome/lighthouse/blob/master/typings/lhr.d.ts
-            // use results.report for the HTML/JSON/CSV output as a string
-            // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
-            return chrome.kill().then(() => results.lhr)
-        });
+        return lighthouse(url, opts, config)
+            .then(results => {
+                console.log(url);
+                // use results.lhr for the JS-consumeable output
+                // https://github.com/GoogleChrome/lighthouse/blob/master/typings/lhr.d.ts
+                // use results.report for the HTML/JSON/CSV output as a string
+                // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
+                return chrome.kill().then(() => results.lhr)
+            });
     });
 }
 
 const opts = {
-    chromeFlags: ['--show-paint-rects', '--headless', '--disable-gpu'],
+    chromeFlags: ['--headless', '--disable-gpu', '--no-sandbox'],
 };
 
 const urls = [
     'https://www.clouty.ru/',
-    'https://www.clouty.ru/trends',
-    'https://www.clouty.ru/shop/women',
-    'https://www.clouty.ru/product-collections',
-    'https://www.clouty.ru/sales',
-    'https://www.clouty.ru/looks',
-    'https://www.clouty.ru/partners',
-    'https://www.clouty.ru/community',
+    // 'https://www.clouty.ru/trends',
+    // 'https://www.clouty.ru/shop/women',
+    // 'https://www.clouty.ru/product-collections',
+    // 'https://www.clouty.ru/sales',
+    // 'https://www.clouty.ru/looks',
+    // 'https://www.clouty.ru/partners',
+    // 'https://www.clouty.ru/community',
 ];
 
 function reportPage(url): Promise<string> {
     return launchChromeAndRunLighthouse(url, opts)
         .then((results: RootObject) => {
             const text = `
-                Page: ${results.requestedUrl}
-                - performance: ${results.categories.performance.score * 100}%
-                - pwa: ${results.categories.pwa.score * 100}%
-                - accessibility: ${results.categories.accessibility.score * 100}%
-                - seo: ${results.categories.seo.score * 100}%
-                - best-practice: ${results.categories['best-practices'].score * 100}%
+                Page: \`${results.requestedUrl}\`
+                - performance: ${Math.round(results.categories.performance.score * 100)}%
+                - pwa: ${Math.round(results.categories.pwa.score * 100)}%
+                - accessibility: ${Math.round(results.categories.accessibility.score * 100)}%
+                - seo: ${Math.round(results.categories.seo.score * 100)}%
+                - best-practice: ${Math.round(results.categories['best-practices'].score * 100)}%
             `;
 
             return text;
